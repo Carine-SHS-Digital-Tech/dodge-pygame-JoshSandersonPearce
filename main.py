3a551af531c2e477bd6e80644ded4b5bfbb2ed84
josh.sanderson-pearc <josh.sanderson-pearc@student.education.wa.edu.au>
2021-09-17 12:29:30 +0800

    Finished Step 2 - 6 I have done half of step 7.

--- main.py
+++ main.py
@@ -5,8 +5,43 @@ import random
 # Define Classes (sprites) here
 class FallingObject(pygame.sprite.Sprite):
     def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.timecreated = pygame.time.get_ticks()
+        self.image = pygame.Surface([30,30])
+        self.image.set_colorkey(black)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0,670)
+        self.rect.y = 0
+    def setImage(self,graphicSelected):
+        fallingObjectsImage = pygame.image.load(graphicSelected)
+        self.image.blit(fallingObjectsImage, (0,0))
+    def moveFallingObjects(self, distance):
+        if self.rect.y <= 470:
+            self.rect.y = self.rect.y + distance
+    def deleteFallingObjects(self):
+        if self.rect.y > 470:
+            self.kill()
+class Character(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([50,68])
+        self.image.set_colorkey(black)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = 310
+        self.rect.y = 420
+        self.image.blit(pygame.image.load("Superhero.png"), (0,0))
+    def moveCharacter(self, movement):
+        if self.rect.x >= 5 and self.rect.x <= 645:
+            self.rect.x = self.rect.x + movement
+        if self.rect.x<5:
+            self.rect.x = 5
+        if self.rect.x>645:
+            self.rect.x = 645
 
 pygame.init()                               # Pygame is initialised (starts running)
+
 
 screen = pygame.display.set_mode([700,500]) # Set the width and height of the screen [width,height]
 pygame.display.set_caption("Dodge")       # Name your window
@@ -17,16 +52,38 @@ black    = (   0,   0,   0)                 # Define some colors using rgb value
 white    = ( 255, 255, 255)                 # used throughout the game instead of using rgb values.
 
 # Define additional Functions and Procedures here
+allFallingObjects = pygame.sprite.Group()
+nextApple = pygame.time.get_ticks() + 2500
 
+charactersGroup = pygame.sprite.Group()
+character = Character()
+charactersGroup.add(character)
+
+movement = 0
 # -------- Main Program Loop -----------
 while done == False:
 
     for event in pygame.event.get():        # Check for an event (mouse click, key press)
         if event.type == pygame.QUIT:       # If user clicked close window
             done = True                     # Flag that we are done so we exit this loop
+        if event.type == pygame.KEYDOWN
+            if event.key == pygame.K_LEFT
+                movement = -5
 
     # Update sprites here
+    if pygame.time.get_ticks() > nextApple:
+        nextObject = FallingObject()
+        nextObject.setImage("Apple.png")
+        nextApple = pygame.time.get_ticks() + 15
+        allFallingObjects.add(nextObject)
+
+    for eachObject in (allFallingObjects.sprites()):
+        eachObject.moveFallingObjects(5)
+
+        eachObject.deleteFallingObjects()
     screen.blit(background_image, [0,0])
+    allFallingObjects.draw(screen)
+    charactersGroup.draw(screen)
     pygame.display.flip()                   # Go ahead and update the screen with what we've drawn.
     clock.tick(20)                          # Limit to 20 frames per second
 
